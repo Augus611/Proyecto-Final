6695b751b422a8275ca904aacb55bc432e3d65ba
juanpaladino15 <juanpaladino15@gmail.com>
2022-06-17 23:46:46 -0300

    Pantalla resgistrar terminada

--- app/src/main/java/com/grupo4/proyectofinal/Activity_Login.kt
+++ app/src/main/java/com/grupo4/proyectofinal/Activity_Login.kt
@@ -1,11 +1,20 @@
 package com.grupo4.proyectofinal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class Activity_Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    //Metodo registrar
+    fun Registrar(view: View?) {
+        val registrar = Intent(this, Registrar::class.java)
+        startActivity(registrar)
+    }
+
 }
